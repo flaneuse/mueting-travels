ce430704c6c7b33878e248bbb54651e3330bbd84
flaneuse <flaneuse@users.noreply.github.com>
2024-09-13 17:27:00 -0700

    add .js

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// leaflet
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+Vue.component('l-map', LMap);
+Vue.component('l-tile-layer', LTileLayer);
+Vue.component('l-marker', LMarker);
+
+// bootstrap
+import { BootstrapVue } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,211 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+
+Vue.use(Vuex);
+
+import {
+  format
+} from "d3-format";
+
+import {
+  nest
+} from "d3-collection";
+
+// basemap from https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html
+// dictionary: https://www.census.gov/programs-surveys/geography/technical-documentation/records-layout/gaz-record-layouts.html
+import COUNTIES from "@/assets/cb_2018_us_county_20m.json";
+import STATES from "@/assets/cb_2018_us_state_20m.json";
+
+
+
+const calcTotal = function(results, person, totalCounties, totalBarWidth = 300) {
+  // calculate non-zero total
+
+  const total = results.map(d => +d[person]).reduce((prev, curr) => curr + prev);
+  const pct = total / totalCounties;
+  const pctFormatted = format(".1%")(pct);
+  const barWidth = pct * totalBarWidth;
+
+  return ({
+    total: total,
+    percent: pct,
+    totalRemaining: totalCounties - total,
+    percentFormatted: pctFormatted,
+    barWidth: barWidth
+  })
+}
+
+
+export default new Vuex.Store({
+  state: {
+    loading: false,
+    people: ["Tim", "Carla", "Sam", "Cody", "John", "Jenny", "Nathan"],
+    geojson: null,
+    states: null,
+    totals: null,
+    stateTotals: null,
+    totalCounties: null,
+    colorPalette: {
+      "all": {
+        color: "#373434",
+        label: "all"
+      },
+      "five_six": {
+        color: "#612e09",
+        label: "5-6 people"
+      },
+      "two_four": {
+        color: "#ac5c22",
+        label: "2-4 people"
+      },
+      "Tim": {
+        color: "#e41a1c",
+        lightColor: "#fdd3d3",
+        label: "Tim"
+      },
+      "Carla": {
+        color: "#3773b8",
+        lightColor: "#cee5ff",
+        label: "Carla"
+      },
+      "Sam": {
+        color: "#ffff33",
+        lightColor: "#ffffc1",
+        label: "Sam"
+      },
+      "Cody": {
+        color: "#4daf4a",
+        label: "Cody"
+      },
+      "John": {
+        color: "#ff7f00",
+        label: "John"
+      },
+      "Jenny": {
+        color: "#984ea3",
+        label: "Jenny"
+      },
+      "Nathan": {
+        color: "#f781bf",
+        label: "Nathan"
+      },
+      "unknown": {
+        color: "#ddddd8",
+        label: "no one"
+      }
+    }
+  },
+  mutations: {},
+  actions: {
+    fetchData(context) {
+      context.state.loading = true;
+
+      const reader = require('g-sheets-api');
+      const readerOptions = {
+        apiKey: process.env.VUE_APP_GOOGLESHEETS_API_KEY,
+        sheetId: '1Rj0M82tUfPT6EtBJB4ydeRqhOk8PLyoQdappyRb-0Fo', // Link to Google sheet. Should have permissions set to "anyone with link can view"
+        sheetName: 'us_counties',
+        returnAllResults: true
+      };
+
+      reader(readerOptions, (results) => {
+        // calculate overall totals by person
+        context.state.totals = {}
+        context.state.totalCounties = results.length;
+        // calculate totals
+        context.state.people.forEach(person => {
+          context.state.totals[person] = calcTotal(results, person, results.length);
+        })
+
+        // calculate totals by state
+        const nested = nest()
+          .key(d => d.state)
+          .rollup(values => {
+            let obj = {}
+
+            context.state.people.forEach(person => {
+              obj[person] = calcTotal(values, person, values.length);
+            })
+
+            return ({
+              total: values.length,
+              people: obj,
+              values: values
+            })
+          })
+          .entries(results);
+
+          nested.forEach(d => {
+            d.total = d.value.total;
+          })
+
+        context.state.stateTotals = nested;
+
+        // process the google sheets: Merge together
+        context.state.geojson = COUNTIES;
+        context.state.states = STATES;
+
+        context.state.geojson["features"].forEach(d => {
+          const filtered = results.filter(row => row.id == d.properties.AFFGEOID);
+          if (filtered.length == 1) {
+            d["properties"]["population"] = filtered[0].population_2019;
+            d["properties"]["state"] = filtered[0].state;
+
+            const Tim = filtered[0].Tim === "1";
+            const Carla = filtered[0].Carla === "1";
+            const Sam = filtered[0].Sam === "1";
+            const Cody = filtered[0].Cody === "1";
+            const John = filtered[0].John === "1";
+            const Jenny = filtered[0].Jenny === "1";
+            const Nathan = filtered[0].Nathan === "1";
+
+            const total = Tim + Carla + Sam + Cody + John + Jenny + Nathan;
+    
+
+            // calculating the color
+            const value = 
+              total > 6 ? "all" :
+              total > 4 ? "five_six" :
+              total > 2 ? "two_four" :
+              Tim ? "Tim" :
+              Carla ? "Carla" :
+              Sam ? "Sam" :
+              Cody ? "Cody" :
+              John ? "John" :
+              Jenny ? "Jenny" :
+              Nathan ? "Nathan" :
+              "unknown";
+
+            d["properties"]["Tim"] = Tim;
+            d["properties"]["Carla"] = Carla;
+            d["properties"]["Sam"] = Sam;
+            d["properties"]["Cody"] = Cody;
+            d["properties"]["John"] = John;
+            d["properties"]["Jenny"] = Jenny;
+            d["properties"]["Nathan"] = Nathan;
+            d["properties"]["fillColor"] = value; //this.colorPalette[value].color;
+          }
+        })
+
+        context.state.loading = false;
+      });
+
+    }
+  },
+  getters: {
+    getStateTotal: (state) => (name) => {
+      return (state.stateTotals ? state.stateTotals.filter(d => d.key == name) : null)
+    },
+    getStateGeojson: (state) => (name) => {
+      if(state.geojson){
+        const geo = state.geojson.features.filter(d => d.properties.state == name);
+      return {features: geo}
+      } else {
+        return(null)
+      }
+
+    }
+  },
+  modules: {},
+});
